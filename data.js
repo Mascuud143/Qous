ca680f2a63ce9ac6f1457cbefa7cbb79b81d87d1
Mascuud <mmascuud61@gmail.com>
2023-07-30 21:21:49 +0200

    Added basic shopping functionality

---
+++ data.js
@@ -0,0 +1,16 @@
+export default  {
+
+    family:{
+        members:[],
+        highlights:[]
+    },
+    shopping:{
+        lists:{
+            week:[{name:"Ost", quantity:2}, {name:"Egg", quantity:1}, {name:"Canbo", quantity:3}, {name:"Eple", quantity:4}],
+            month:[]
+        }
+    },
+    weather:[]
+}
+
+
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // DOM
 const hourBox = document.querySelector(".date-hour")
 const monthBox = document.querySelector(".date-month")
@@ -8,22 +7,73 @@ const closeModalBtn = document.querySelectorAll(".modal-close-btn")
 const familyModal = document.querySelector(".modal-family")
 const homeModal = document.querySelector(".modal-home")
 const shoppingModal = document.querySelector(".modal-shopping")
+const reminderModal = document.querySelector(".modal-reminder")
 const familyBtn = document.querySelector(".btn-family")
 const HomeBtn = document.querySelector(".btn-home")
 const shoppingBtn = document.querySelector(".shopping-btn")
-
+const reminderBtn = document.querySelector(".reminder-btn")
+const openAddMember = document.querySelector(".open-add-member")
+const addMemberModal = document.querySelector(".modal-mini")
+const closeAddMember = document.querySelector(".close-add-member")
+const AddMemberBtn = document.querySelector(".add-member-btn")
+const familyList = document.querySelector(".family-list")
+const expandListBtn = document.querySelectorAll(".expand-list")
+const addItemModal = document.querySelector(".add-item")
+const newItemBtn = document.querySelector(".new-item-btn")
+const closeAddItemBtn = document.querySelector(".close-add-item")
+const decreaseQuantity = document.querySelector(".less")
+const increaseQuantity = document.querySelector(".more")
+const quantityBox = document.querySelector(".quantity-box")
+const listItems = document.querySelector(".list-items")
+const addListForm = document.querySelector(".add-item form")
+const addMoreBtns = document.querySelectorAll(".add-more")
+const closeShopBtn = document.querySelector(".close-shop")
+const shopList = document.querySelector(".shop-list")
+const shopperListOption = document.querySelector(".shop select")
 
 
 
 
 const state = {
 
-    
+    family:{
+        members:[],
+        highlights:[]
+    },
+    shopping:{
+        lists:{
+            week:[{name:"Ost", quantity:2}, {name:"Egg", quantity:1, description:"Mida box ga"}, {name:"Canbo", quantity:3}, {name:"Eple", quantity:4}],
+            month:[{name:"Hvetemel", quantity:20}]
+        }
+    }
 }
+
+
 
 var monthArray= ["January","February","March","April","May","June","July",
             "August","September","October","November","December"];
 const days=["Søndag","Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"]
+
+
+
+function updateMemberList(){
+    familyList.innerHTML=""
+    if(state.family.members.length!=0){
+
+        const html = state.family.members.map(el=> `<div class="family-member"><h3>${el.name}</h3></div>`).join(" ")
+        familyList.insertAdjacentHTML("afterbegin", html)
+        document.querySelectorAll(".family-member").forEach(el=> el.addEventListener("click", function(e){
+            // Do this if 
+            // <p>${el.phone}</p>
+        }))
+    }else{
+        familyList.textContent="There are currently no members!"
+    }
+
+
+}
+
+
 
 
 
@@ -104,7 +154,6 @@ async function getWeatherData(city){
     fetch(link).then(res=> res.json().then(data=>{
         const weatherNow = Math.round((data.data.timelines[0].intervals[0].values.temperature))
         temperature.textContent = weatherNow+" "+"℃";
-        console.log(data.data.timelines[0].intervals[0].values.temperature)
     
 
         
@@ -112,7 +161,6 @@ async function getWeatherData(city){
         nextdays.day2.temp = Math.round(data.data.timelines[0].intervals[26+24].values.temperature)
         nextdays.day3.temp = Math.round(data.data.timelines[0].intervals[26+(24*2)].values.temperature)
 
-        console.log(nextdays)
 
         document.querySelector(".day1").innerHTML = getNextDays()[0]+`<span>${nextdays.day1.temp} ℃</span>`
         document.querySelector(".day2").innerHTML = getNextDays()[1]+`<span>${nextdays.day2.temp} ℃</span>`
@@ -129,21 +177,226 @@ async function getWeatherData(city){
 window.addEventListener("load", function(e){
 
     // Load weather data
+    
 
+    document.querySelectorAll(".coming-soon").forEach(el=> el.innerHTML="Coming soon")
+    getWeatherData()
+    UpdateListData()
+    document.querySelector(".list-items-count").textContent= state.shopping.lists.week.length +" "+"Items";
+    deleteItemEvent()
+})
+
+
+addMoreBtns.forEach(el=>{
+    el.addEventListener("click", openAddList)
+
+})
+
+addListForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const formData = new FormData(addListForm)
+
+    const listData = {
+        name:e.target.name.value,
+        quantity: Number(e.target.querySelector(".quantity-box").textContent.trim()),
+        description:e.target.description.value,
+    }
+
+
+    const listType = [...e.target.parentNode.classList]
+    if(e.target.parentNode.dataset.listType=='week'){
+        state.shopping.lists.week.push(listData)
+    }else{
+        
+        state.shopping.lists.month.push(listData)
+    }
+
+
+
+    UpdateListData()
+
+
+    //close mini modal
+    closeAddItem()
+})
+
+
+function deleteItemEvent(){
+
+
+    document.querySelectorAll(".list-item i").forEach(el=>{
+        el.addEventListener("click", function(e){
+            
+            // is week or month
+            const listType = [...e.target.parentNode.parentNode.parentNode.parentNode.classList]
+            console.log(listType.includes("week-list"))
+
+            if(listType.includes("week-list")){
+
+                
+                state.shopping.lists.week = state.shopping.lists.week.filter((el,i)=> i!=e.target.parentNode.parentNode.dataset.id)
+            }else{
+                
+                state.shopping.lists.month = state.shopping.lists.month.filter((el,i)=> i!=e.target.parentNode.parentNode.dataset.id)
+            }
+
+            UpdateListData()
+        })
+    })
+}
+
+
+function UpdateListData(){
+
+
+    const weekHtml = state.shopping.lists.week.map((el, i)=>{
+        const descr= `<p class='item-description'>${el.description}</p>`
+        return `<div class="list-item" data-id="${i}"> <div class="item-content"><span>${el.quantity} x </span> ${el.name} <i class="fa-solid fa-trash"></i></div> ${el.description?descr:""}</div>`
+    }).join(" ")
+
+    const monthHtml = state.shopping.lists.month.map((el, i)=>{
+        const descr= `<p class='item-description'>${"**"+el.description}</p>`
+        return `<div class="list-item" data-id="${i}"> <div class="item-content"><span>${el.quantity} x </span> ${el.name} <i class="fa-solid fa-trash"></i></div> ${el.description?descr:""}</div>`
+    }).join(" ")
+
+
+    const weekListNames = state.shopping.lists.week.slice(0, 3).map(el=> el.name).join(", ")+" "+"..."
+    const monthListNames = state.shopping.lists.month.slice(0, 3).map(el=> el.name).join(", ")+" "+"..."
+
+
+
+    document.querySelectorAll(".list-desc").forEach(el=>el.textContent="")
+    document.querySelector(".week-list .list-desc").textContent= weekListNames
+    document.querySelector(".month-list .list-desc").textContent= monthListNames
+    document.querySelector(".list-desc").textContent= weekListNames
+    document.querySelector(".week-list .list-items").innerHTML=""
+    document.querySelector(".month-list .list-items").innerHTML=""
+
+    document.querySelector(".week-list .list-items").insertAdjacentHTML("afterbegin", weekHtml)
+    document.querySelector(".month-list .list-items").insertAdjacentHTML("afterbegin", monthHtml)
+    document.querySelector(".week-list .list-items-count").textContent= state.shopping.lists.week.length +" "+"Items";
+    document.querySelector(".month-list .list-items-count").textContent= state.shopping.lists.month.length +" "+"Items";
+    deleteItemEvent()
+}
+
+
+closeShopBtn.addEventListener("click", function(e){
+    document.querySelector(".shop").classList.add("hidden")
+})
+
+shopList.addEventListener("click", function(e){
+    shopperListOption.innerHTML=""
+    document.querySelector(".shop").classList.remove("hidden")
+    
+    const html = state.family.members.map(el=> `<option value="${el.name}">${el.name}</option>`)
+    shopperListOption.insertAdjacentHTML("afterbegin", html)
+
+})
+
+newItemBtn.addEventListener("click", openAddList)
+closeAddItemBtn.addEventListener("click", closeAddItem)
+
+decreaseQuantity.addEventListener("click", function(e){
+    var count = Number(quantityBox.textContent.trim())
+    if(count>1){
+        count=count-1
+        updateQuantity(count)
+    }
+})
+increaseQuantity.addEventListener("click", function(e){
+    var count = Number(quantityBox.textContent.trim())
+    count=count+1
+    updateQuantity(count)
+})
+
+function updateQuantity(count){
+    quantityBox.textContent = count
+}
+expandListBtn.forEach(el=>{
+    el.addEventListener("click", function(e){
+
+        
+        // show the list 
+        e.target.parentNode.parentNode.querySelector(".list-items").classList.toggle("hidden")
+        
+        
+        // remove the description
+        
+        const isOpen =  [...(e.target.parentNode.parentNode.querySelector(".list-items").classList)].includes("hidden")
+        
+        if(isOpen){
+            e.target.parentNode.parentNode.querySelector(".list-desc").classList.remove("hidden")
+            // change the icon
+    
+            e.target.parentNode.parentNode.querySelector(".expand-list").innerHTML = `<i class="fa-solid fa-angle-right"></i>`
+            
+            
+        }else{
+            
+            e.target.parentNode.parentNode.querySelector(".list-desc").classList.add("hidden")
+            e.target.parentNode.parentNode.querySelector(".expand-list").innerHTML = `<i class="fa-solid fa-angle-up"></i>`
+            // e.target.parentNode.parentNode.querySelector(".list-desc").classList.add("hidden")
+    
+        }
+    
+    
+    })
+})
+
+openAddMember.addEventListener("click",  function(e){
+    addMemberModal.classList.remove("hidden")
+})
+
+closeAddMember.addEventListener("click", closeMiniModal)
+
+AddMemberBtn.addEventListener("click", function(e){
+    e.preventDefault()
+
+    const memberName = document.querySelector(".member-name").value
+    const phone = "1234567"
+
+    state.family.members.push({name:memberName, phone:phone})
+
+
+    closeMiniModal()
+    updateMemberList()
+})
+
+
+function openAddList(e){
+    addItemModal.classList.remove("hidden")
+
+    const parentList  = [...(e.target.parentNode.parentNode.classList)]                                                                                                             
+
+    console.log(parentList)
+
+    let isWeek = parentList.includes("week-list");
+    let isMonth = parentList.includes("month-list");
+
+    if(isWeek){
+       
+        addItemModal.dataset.listType = "week"                                                                                                            
+    }else if(isMonth){
+        
+        addItemModal.dataset.listType = "month"                                                                                                            
+    }
     
 
 
-})
-getWeatherData()
+
+
+}
+
+function closeAddItem(){
+    addItemModal.classList.add("hidden")
+}
+
 function getNextDays(){
 
     let date = new Date().getDay()
 
-    console.log(date)
     if(date==4){
-        console.log("hh")
     }
-    console.log(date+3)
     return [days[date+1], days[date+2], days[date+3]]
 }
 
@@ -151,10 +404,13 @@ function getNextDays(){
 closeModalBtn.forEach(btn=>{
     btn.addEventListener("click", function(e){
         closeModals()
+        closeMiniModal()
     })
 })
 familyBtn.addEventListener("click", function(){
     familyModal.classList.remove("hidden")
+    updateMemberList()
+
 })
 HomeBtn.addEventListener("click", function(){
     homeModal.classList.remove("hidden")
@@ -162,6 +418,9 @@ HomeBtn.addEventListener("click", function(){
 shoppingBtn.addEventListener("click", function(){
     shoppingModal.classList.remove("hidden")
 })
+// reminderBtn.addEventListener("click", function(){
+//     reminderModal.classList.remove("hidden")
+// })
 
 setInterval(()=>{
 
@@ -172,4 +431,11 @@ setInterval(()=>{
 
 function closeModals(){
     document.querySelectorAll(".modal").forEach(el=> el.classList.add("hidden"))
+}
+
+function closeMiniModal(){
+
+        addMemberModal.classList.add("hidden")
+    
+    
 }
